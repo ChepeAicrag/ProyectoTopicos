b4764fb7eb073c4338157b67717403ab8beb7971
JoseAngel <19angelgarcia00@gmail.com>
2020-04-13 18:44:12 -0500

    Proyecto
    Se agregó la clase para hacer la conexion con la BD, de igual forma se dividió el proyecto en paquetes y tambien se adjunta la clase Vista_Registro

---
+++ Proyecto_01/src/MVC_Principal/PrincipalProyecto.java
@@ -0,0 +1,15 @@
+package MVC_Principal;
+
+import Modelo.Conexion;
+import Vista.Vista_Registro;
+
+/**
+ * 
+ * @author García García José Ángel
+ */
+public class PrincipalProyecto {
+    public static void main(String[] args) {
+        //Modelo.Conexion conexion = new Conexion("Mezcalera");
+        new Vista_Registro();
+    }
+}
---
+++ Proyecto_01/src/Vista/Vista_Login.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Vista;
+
+import javax.swing.*;
+
+/**
+ * 
+ * @author García García José Ángel
+ */
+public class Vista_Login extends JFrame implements Runnable{
+    
+    private JLabel etqUsuario;
+    private JLabel etqContraseña;
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
