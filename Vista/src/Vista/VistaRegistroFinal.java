fd0bff13284fef7735b7d13e5283a95358c2e9ba
JoseAngel <19angelgarcia00@gmail.com>
2020-05-31 00:02:17 -0500

    Agregando Vista-Informe
    Se ha adjuntado una nueva vista y su modelo de tabla que utiliza.

---
+++ Vista/src/Vista/VistaRegistroFinal.java
@@ -0,0 +1,79 @@
+/*
+ * Para la informacion final de las tandas
+ */
+
+package Vista;
+
+import Modelo.ModeloTablaInforme;
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+
+/**
+ * 
+ * @author García García José Ángel
+ */
+public class VistaRegistroFinal extends JPanel{
+    
+    public ModeloTablaInforme mti;
+    public JTable tabla;
+    private JScrollPane scroll;
+    private JLabel titulo;
+    public JButton btnSalir; // Solo para cerrar la BD
+    private DefaultTableCellRenderer dtcr;
+    private JTableHeader hTab ;
+    
+    public VistaRegistroFinal(){
+        setSize(1020, 680);
+        setVisible(true);
+        colocar();
+        revalidate();
+    }
+    
+    public void colocar(){
+        SpringLayout s = new SpringLayout();
+        setLayout(s);
+        titulo = new JLabel("REGISTRO DE LAS TANDAS PRODUCIDAS");
+        titulo.setAlignmentX(SwingUtilities.CENTER);
+        add(titulo);
+        s.putConstraint(SpringLayout.NORTH, titulo, 12, SpringLayout.NORTH, this);
+        s.putConstraint(SpringLayout.WEST, titulo,465, SpringLayout.WEST, this);
+        s.putConstraint(SpringLayout.EAST, titulo, -470, SpringLayout.EAST, this);
+        mti = new ModeloTablaInforme();
+        tabla = new JTable(mti);
+        dtcr = new DefaultTableCellRenderer();
+        dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+        tabla.setDefaultRenderer(String.class, dtcr);
+        hTab = tabla.getTableHeader();
+        hTab.setAlignmentX(JTableHeader.CENTER_ALIGNMENT);
+        hTab.setResizingAllowed(false);
+        hTab.setReorderingAllowed(false);
+        hTab.setBorder(BorderFactory.createLineBorder(Color.yellow, 1));
+        hTab.setBackground(Color.BLACK);
+        hTab.setForeground(Color.WHITE);
+        add(hTab);
+        //s.putConstraint(SpringLayout.NORTH, hTab, 12, SpringLayout.SOUTH, titulo);
+        //s.putConstraint(SpringLayout.WEST, hTab, 300, SpringLayout.WEST, this);
+        //s.putConstraint(SpringLayout.EAST, hTab, -310, SpringLayout.EAST, this);
+        add(tabla);
+        scroll = new JScrollPane();
+        scroll.setViewportView(tabla);
+        scroll.setBackground(Color.BLACK);
+        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.setAlignmentX(JScrollPane.CENTER_ALIGNMENT);
+        add(scroll);
+        s.putConstraint(SpringLayout.NORTH, scroll, 12, SpringLayout.SOUTH, hTab);
+        s.putConstraint(SpringLayout.WEST, scroll, 2, SpringLayout.WEST, this);
+        s.putConstraint(SpringLayout.EAST, scroll, -2, SpringLayout.EAST, this);
+    }
+}
