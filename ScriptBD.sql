7f3cebf24c8f7035d61cc276d34938d2147bc459
JoseAngel <19angelgarcia00@gmail.com>
2020-05-31 00:01:04 -0500

    Scritp
    Le falta ajustar unas cosas, pero ya es un avance

--- ScriptBD.sql
+++ ScriptBD.sql
@@ -1,9 +1,57 @@
-﻿/**create schema Mezcal authorization postgres;*/
+﻿create schema Mezcal authorization postgres;
 set search_path to Mezcal;
 
+create table Maguey(
+ id_maguey serial primary key,
+ nombre varchar(30),
+ cantidadPinia int
+);
+
+create table Cortador(
+ id_cortador serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
+create table Horno(
+ id_horno serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
+create table Molino(
+ id_molino serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
+create table Fermentador(
+ id_fermentador serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
+create table Destilador(
+ id_destilador serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
+create table Enbotelladora(
+ id_enbotelladora serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
+create table Cliente(
+ id_cliente serial primary key,
+ nombre varchar(30),
+ estado varchar(30)
+);
+
 create table gradoAlcohol(
-id_grado int primary key,
-valor float8
+ id_grado int primary key,
+ valor float8
 );
 
 create table tipoMezcal(
@@ -12,12 +60,19 @@ nombre varchar(20)
 );
 
 create table Tanda(
-id_tanda int primary key,
-tipoMaguey int references Maguey(id_maguey),
-gradoAlcohol int references gradoAlcohol(id_grado),
-tipoMezcal int references tipoMezcal(id_tipo),
-cantidadPinias int,
-status varchar(20),
-fecha_inicio date,
-fecha_final date
+ id_tanda serial primary key,
+ tipoMaguey int references Maguey(id_maguey),
+ gradoAlcohol int references gradoAlcohol(id_grado),
+ tipoMezcal int references tipoMezcal(id_tipo),
+ cantidadPinias int,
+ status varchar(20),
+ id_cortador serial references Cortador(id_cortador),
+ id_horno int references Horno(id_horno),
+ id_molino int references Molino(id_molino),
+ id_fermentador int references Fermentador(id_fermentador),
+ id_destilador int references Destilador(id_destilador),
+ id_enbotelladora int references Enbotelladora(id_enbotelladora),
+ id_cliente int references Cliente(id_cliente),
+ fecha_inicio date,
+ fecha_final date
 );
